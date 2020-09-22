25a437ba3bc97860b74b2d8db756c4d68371f72f
luciano-compromiso <71734372+luciano-compromiso@users.noreply.github.com>
2020-09-22 18:49:05 -0300

    Add files via upload

---
+++ clase3/js/script.js
@@ -0,0 +1,17 @@
+console.log("Hola Mundo!");
+console.log("Este es el curso");
+console.log("de Javascript");
+
+var nombre = "Luciano";
+var apellido = "Menez";
+var edad = 30;
+
+
+function alerta() {
+  alert("Apretaste el botón!");
+}
+
+function cambiar_color() {
+  var x = document.getElementById("titulo");
+  x.style.color = "red";
+}
